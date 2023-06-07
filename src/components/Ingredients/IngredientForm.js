2450ae91f08c7377203fb6b5cbaeccb66de2f3b7
Thiago Leite <thiago.leite@agileengine.com>
2023-06-07 16:15:51 -0300

    Understanding useState

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
-const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
+const IngredientForm = React.memo((props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+
+  const inputTitleHandler = (event) => {
+    const newTitle = event.target.value;
+
+    setEnteredTitle(newTitle);
+  };
+
+  const inputAmountHanlder = (event) => {
+    const newAmount = event.target.value;
+
+    setEnteredAmount(newAmount);
+  };
+
+  const submitHandler = (event) => {
     event.preventDefault();
     // ...
   };
@@ -15,11 +30,21 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input
+              type="text"
+              id="title"
+              value={enteredTitle}
+              onChange={inputTitleHandler}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input
+              type="number"
+              id="amount"
+              value={enteredAmount}
+              onChange={inputAmountHanlder}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
