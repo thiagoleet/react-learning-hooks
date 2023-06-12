5a8e107ee188de5018c6a35b700ae3a49129b661
Thiago Leite <thiago.leite@agileengine.com>
2023-06-12 09:55:01 -0300

    Cleaning up with useEffect

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -10,7 +10,7 @@ const Search = React.memo((props) => {
   const inputRef = useRef();
 
   useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       if (enteredFilter === inputRef.current.value) {
         const query =
           enteredFilter.length === 0
@@ -33,6 +33,11 @@ const Search = React.memo((props) => {
           });
       }
     }, 500);
+
+    // clean up function
+    return () => {
+      clearTimeout(timer);
+    };
   }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
