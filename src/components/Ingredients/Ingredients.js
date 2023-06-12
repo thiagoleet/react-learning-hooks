44013eb4818c6857075bf11239b38774fda65bcb
Thiago Leite <thiago.leite@agileengine.com>
2023-06-12 10:36:20 -0300

    Handling errors

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
 import { addIngredient, removeIngredient } from "../../util";
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     console.log("RENDERING INGREDIENTS", userIngredients);
@@ -31,22 +33,33 @@ const Ingredients = () => {
   const removeIngredientHandler = (ingredientId) => {
     setIsLoading(true);
 
-    removeIngredient(ingredientId).then(() => {
-      setIsLoading(false);
-      setUserIngredients((prevIngredients) => {
-        return prevIngredients.filter(
-          (ingredient) => ingredient.id !== ingredientId
-        );
+    removeIngredient(ingredientId)
+      .then(() => {
+        setIsLoading(false);
+        setUserIngredients((prevIngredients) => {
+          return prevIngredients.filter(
+            (ingredient) => ingredient.id !== ingredientId
+          );
+        });
+      })
+      .catch((error) => {
+        setError("Something went wrong");
       });
-    });
   };
 
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
     setUserIngredients(filteredIngredients);
   }, []);
 
+  const clearError = () => {
+    setError(null);
+    setIsLoading(false);
+  };
+
   return (
     <div className="App">
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+
       <IngredientForm
         onAddIngredient={addIngredientHandler}
         loading={isLoading}
