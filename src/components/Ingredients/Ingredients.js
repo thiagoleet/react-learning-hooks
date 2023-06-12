260174f585f0da5388022fb461f2592e8060d4ef
Thiago Leite <thiago.leite@agileengine.com>
2023-06-12 10:49:39 -0300

    Using useReducer intead of useState

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useReducer, useState, useEffect, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -6,8 +6,30 @@ import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
 import { addIngredient, removeIngredient } from "../../util";
 
+const ingredientActions = {
+  SET_INGREDIENTS: "SET",
+  ADD_INGREDIENT: "ADD",
+  DELETE_INGREDIENT: "DELETE",
+};
+
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case ingredientActions.SET_INGREDIENTS:
+      return action.ingredients;
+    case ingredientActions.ADD_INGREDIENT:
+      return [...currentIngredients, action.ingredient];
+    case ingredientActions.DELETE_INGREDIENT:
+      return currentIngredients.filter(
+        (ingredient) => ingredient.id !== action.id
+      );
+    default:
+      throw new Error("Should not get there!");
+  }
+};
+
 const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([]);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  // const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
@@ -20,13 +42,21 @@ const Ingredients = () => {
 
     addIngredient(ingredient).then((data) => {
       setIsLoading(false);
-      setUserIngredients((prevIngredients) => [
-        ...prevIngredients,
-        {
+      // setUserIngredients((prevIngredients) => [
+      //   ...prevIngredients,
+      //   {
+      //     id: data.name,
+      //     ...ingredient,
+      //   },
+      // ]);
+
+      dispatch({
+        type: ingredientActions.ADD_INGREDIENT,
+        ingredient: {
           id: data.name,
           ...ingredient,
         },
-      ]);
+      });
     });
   };
 
@@ -36,10 +66,15 @@ const Ingredients = () => {
     removeIngredient(ingredientId)
       .then(() => {
         setIsLoading(false);
-        setUserIngredients((prevIngredients) => {
-          return prevIngredients.filter(
-            (ingredient) => ingredient.id !== ingredientId
-          );
+        // setUserIngredients((prevIngredients) => {
+        //   return prevIngredients.filter(
+        //     (ingredient) => ingredient.id !== ingredientId
+        //   );
+        // });
+
+        dispatch({
+          type: ingredientActions.DELETE_INGREDIENT,
+          id: ingredientId,
         });
       })
       .catch((error) => {
@@ -49,7 +84,11 @@ const Ingredients = () => {
   };
 
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
-    setUserIngredients(filteredIngredients);
+    // setUserIngredients(filteredIngredients);
+    dispatch({
+      type: ingredientActions.SET_INGREDIENTS,
+      ingredients: filteredIngredients,
+    });
   }, []);
 
   const clearError = () => {
